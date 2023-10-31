d652c313667b29b325bab6dcdef371dbb1258102
mjd10m <mjd10m@outlook.com>
2023-10-30 22:19:20 -0400

    routes and single thought components finished

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,5 +15,22 @@ export const QUERY_THOUGHTS = gql`
             reactionBody
         }
         }
+    }`
+
+    export const QUERY_THOUGHT = gql`
+    query thought($id: ID!) {
+      thought(_id: $id) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+        reactions {
+          _id
+          createdAt
+          username
+          reactionBody
+        }
+      }
     }
-    `
+  `;
